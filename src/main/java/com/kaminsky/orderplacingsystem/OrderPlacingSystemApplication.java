03ef15452d20ca1a2bc7eef622bbe5957a7b40b7
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-05 19:35:59 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/orderplacingsystem/OrderPlacingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kaminsky.orderplacingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlacingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPlacingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaminsky/orderplacingsystem/OrderPlacingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.orderplacingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlacingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
